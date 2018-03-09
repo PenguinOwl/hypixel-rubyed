a8e8945ccdb1a968f7cb1a8cb9b8c2f725a1999b
Henry <henryjboles@gmail.com>
2018-03-09 09:37:19 -0800

    Update hypixel-ruby.rb

--- lib/hypixel-ruby.rb
+++ lib/hypixel-ruby.rb
@@ -1,15 +1,21 @@
 require 'open-uri'
 require 'json'
 
+# Base object for the api. Only create one for the entire build as it will one day keep track of your request limits. *All methods are avaible at https://github.com/HypixelDev/PublicAPI/tree/master/Documentation/methods, use arguments are parms.*
 class HypixelAPI
   
+  # Parses url to Rubyfiy the request, internal so you won't need to use it for much.
   def fetch(url)
     source = (open URI(url)).read
     return JSON.parse(source, :symbolize_names => true)
   end
+  
+  # Creates a new object with your API key. *If request are failing for you, remember to check your key.*
   def initialize(key)
     @key = key
   end
+  
+  # Builds a url from the request type and parameters. *Do not specify your key in the parameters.*
   def url(type, hash)
     url = "https://api.hypixel.net/"
     url << type.to_s
@@ -28,30 +34,37 @@ class HypixelAPI
 # ex: api.player( :uuid => "<uuid here>" )
 #
   
+  # Fetches boosters and their active times.
   def boosters(args={})
     fetch(url(:"boosters", args))
   end
   
+  # Finds a guild by name or player.
   def findguild(args={})
     fetch(url(:"findguild", args))
   end
   
+  # Refer to PublicAPI docs on this one.
   def friends(args={})
     fetch(url(:"friends", args))
   end
   
+  # Fetches guild data by id.
   def guild(args={})
     fetch(url(:"guild", args))
   end
   
+  # Fetches info about current status of API key.
   def key(args={})
     fetch(url(:"key", args))
   end
   
+  # Gets top players in each game.
   def leaderboards(args={})
     fetch(url(:"leaderboards", args))
   end
   
+  # Fetches player, recommended that you webscrape for uuid before finding them.
   def player(args={})
     fetch(url(:"player", args))
   end
@@ -60,6 +73,7 @@ class HypixelAPI
     fetch(url(:"session", args))
   end
   
+  # Gets info on bans/watchdog.
   def watchdogstats(args={})
     fetch(url(:"watchdogstats", args))
   end
